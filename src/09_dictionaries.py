7b265d87de5ca25f084ca2cf5e509870d18c45bd
Daanish Rasheed <daanishrasheed@gmail.com>
2020-05-05 00:46:41 -0400

    more done

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,13 +35,22 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
-
+waypoints.append({
+    "lat": 20,
+    "lon": -154,
+    "name": "a fourth place"
+})
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
-# YOUR CODE HERE
+for i in waypoints:
+    if i["name"] == "a place":
+        i["lon"] =  -130
+        i["name"] = "not a real place"
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for i in waypoints:
+    print(i.values())
--- src/11_args.py
+++ src/11_args.py
@@ -6,6 +6,9 @@
 
 # YOUR CODE HERE
 
+def f1(num1, num2):
+    return num1 + num2
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -13,6 +16,12 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+def f2(*num):
+    sum = 0
+    for i in num:
+        sum = sum + i
+    return sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -31,6 +40,10 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+def f3(num1, num2 = 1):
+    return num1 + num2
+
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,6 +57,8 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(*args):
+    print(args)
 
 # Should print
 # key: a, value: 12
